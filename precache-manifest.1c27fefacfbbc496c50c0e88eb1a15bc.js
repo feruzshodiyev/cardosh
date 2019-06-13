4457b0b0205cc64eef413e85bc4075d6b4361294
feruzshodyev <feruzshodiyev1009@mail.ru>
2019-06-13 13:18:08 +0500

    Updates

---
+++ precache-manifest.1c27fefacfbbc496c50c0e88eb1a15bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ce50e8e0c6c161bedc0d4d82d074d7",
+    "url": "feruzshodiyev.github.io/cardosh/index.html"
+  },
+  {
+    "revision": "bb2f3b07e10b7a48ae95",
+    "url": "feruzshodiyev.github.io/cardosh/static/css/2.d08b99d3.chunk.css"
+  },
+  {
+    "revision": "aa7f548ed2d6dd433da8",
+    "url": "feruzshodiyev.github.io/cardosh/static/css/main.2c507507.chunk.css"
+  },
+  {
+    "revision": "bb2f3b07e10b7a48ae95",
+    "url": "feruzshodiyev.github.io/cardosh/static/js/2.51cafe82.chunk.js"
+  },
+  {
+    "revision": "aa7f548ed2d6dd433da8",
+    "url": "feruzshodiyev.github.io/cardosh/static/js/main.7d965233.chunk.js"
+  },
+  {
+    "revision": "2ecd267fb47b3de41beb",
+    "url": "feruzshodiyev.github.io/cardosh/static/js/runtime~main.56ccddc7.js"
+  }
+]);
